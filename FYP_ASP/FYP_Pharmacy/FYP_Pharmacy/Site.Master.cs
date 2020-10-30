d1f6367437ec43b1c0885a39796aeb7fb31bc937
WaleedKhan077 <waleedmuhammad728@gmail.com>
2020-10-30 23:58:32 +0500

    Nav bar changes

--- FYP_ASP/FYP_Pharmacy/FYP_Pharmacy/Site.Master.cs
+++ FYP_ASP/FYP_Pharmacy/FYP_Pharmacy/Site.Master.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Web.UI;
 
 namespace FYP_Pharmacy
@@ -7,7 +8,38 @@ namespace FYP_Pharmacy
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                if (Session != null || Session[Generics.Enums.SessionName.UserDetails.ToString()] != null)
+                {
+                    Response.Redirect("login.aspx");
+                }
+                else
+                {
+                    DataTable dt = (DataTable)Session[Generics.Enums.SessionName.UserDetails.ToString()];
+                    if (string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) || dt.Rows[0]["accesslevel"].ToString() == "1001")
+                    {
+                        medPage.Visible = true;
+                        pharmacompanypage.Visible = true;
+                        pharmacypage.Visible = true;
+                        userspage.Visible = true;
+                    }
+                    else if (string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) || dt.Rows[0]["accesslevel"].ToString() == "1002")
+                    {
+                        medPage.Visible = false;
+                        pharmacompanypage.Visible = false;
+                        pharmacypage.Visible = false;
+                        userspage.Visible = false;
+                    }
+                    else if (string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) || dt.Rows[0]["accesslevel"].ToString() == "1003")
+                    {
+                        medPage.Visible = false;
+                        pharmacompanypage.Visible = false;
+                        pharmacypage.Visible = false;
+                        userspage.Visible = false;
+                    }
+                }
+            }
         }
     }
 }
