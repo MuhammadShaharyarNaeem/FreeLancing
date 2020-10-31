d8807bde1cc81d783d1b1368776ce92db12d0ea0
WaleedKhan077 <waleedmuhammad728@gmail.com>
2020-10-31 19:27:22 +0500

    Navbar pages change

--- FYP_ASP/FYP_Pharmacy/FYP_Pharmacy/Site.Master.cs
+++ FYP_ASP/FYP_Pharmacy/FYP_Pharmacy/Site.Master.cs
@@ -8,38 +8,50 @@ namespace FYP_Pharmacy
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            if (Session != null)
             {
-                if (Session != null || Session[Generics.Enums.SessionName.UserDetails.ToString()] != null)
+                DataTable dt = (DataTable)Session[Generics.Enums.SessionName.UserDetails.ToString()];
+                if (dt != null && dt.Rows.Count > 0)
                 {
-                    Response.Redirect("login.aspx");
-                }
-                else
-                {
-                    DataTable dt = (DataTable)Session[Generics.Enums.SessionName.UserDetails.ToString()];
-                    if (string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) || dt.Rows[0]["accesslevel"].ToString() == "1001")
+                    if (!string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) && dt.Rows[0]["accesslevel"].ToString() == "1001")
                     {
                         medPage.Visible = true;
                         pharmacompanypage.Visible = true;
                         pharmacypage.Visible = true;
                         userspage.Visible = true;
+                        pharmaInventory.Visible = true;
+                        PosPage.Visible = true;
                     }
-                    else if (string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) || dt.Rows[0]["accesslevel"].ToString() == "1002")
+                    else if (!string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) && dt.Rows[0]["accesslevel"].ToString() == "1002")
+                    {
+                        medPage.Visible = true;
+                        pharmacompanypage.Visible = false;
+                        pharmacypage.Visible = false;
+                        userspage.Visible = false;
+                        pharmaInventory.Visible = false;
+                        PosPage.Visible = false;
+                    }
+                    else if (!string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) && dt.Rows[0]["accesslevel"].ToString() == "1003")
                     {
                         medPage.Visible = false;
                         pharmacompanypage.Visible = false;
                         pharmacypage.Visible = false;
                         userspage.Visible = false;
+                        pharmaInventory.Visible = true;
+                        PosPage.Visible = true;
                     }
-                    else if (string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) || dt.Rows[0]["accesslevel"].ToString() == "1003")
+                    else if (!string.IsNullOrWhiteSpace(dt.Rows[0]["accesslevel"].ToString()) && dt.Rows[0]["accesslevel"].ToString() == "1004")
                     {
                         medPage.Visible = false;
                         pharmacompanypage.Visible = false;
-                        pharmacypage.Visible = false;
+                        pharmacypage.Visible = true;
                         userspage.Visible = false;
+                        pharmaInventory.Visible = false;
+                        PosPage.Visible = true;
                     }
                 }
             }
+
         }
     }
 }
